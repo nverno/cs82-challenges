714e2d5228007b551b0fe7e58d065aa65f16075b
nverno <noah.v.peart@gmail.com>
2019-12-08 18:42:53 -0500

    maybe refactor and finish

--- test_action.py
+++
@@ -1,4 +0,0 @@
-from state_estimation import *
-
-print(initial_policy[0][0])
-
--- test_r.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-
-from numpy.random import choice
-import timeit
-
-timeit.timeit("choice(1)", number=10000)
